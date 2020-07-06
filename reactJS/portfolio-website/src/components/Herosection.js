7a7617f2a17f351f7bc7e8deb1bb1b85498d87a2
rupaku <rupakumari201@yahoo.com>
2020-07-07 01:03:31 +0530

    portfolio website

---
+++ reactJS/portfolio-website/src/components/Herosection.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Herosection = () => {
+  return (
+    <section
+      id="hero"
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
+      <div className="hero-container" data-aos="fade-in">
+        <h1>Alex Smith</h1>
+        <p>
+          I'm
+          <span
+            className="typed"
+            data-typed-items="Designer, Developer, Freelancer, Photographer"
+          ></span>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Herosection
